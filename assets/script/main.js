1053ef921078d7f6d3103b6e43ebf10e9f9be8e6
ismail <ismaildimah@gmail.com>
2026-02-08 23:46:21 +0100

    Ajout de jeux des monnaies

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,4 @@
-import {switchCurrency,getCurrency} from "./fonction.js";
+import { switchCurrency, getCurrency } from "./fonction.js";
 let formSwitch = document.querySelector("#formSwitch");
 let formConvert = document.querySelector("#formConvert");
 let result = document.querySelector("#result");
@@ -6,7 +6,121 @@ let devises = document.querySelectorAll(".devise");
 let devise1 = devises[0];
 let devise2 = devises[1];
 
+try {
 
-switchCurrency(formSwitch,devise1,devise2);
 
-getCurrency(formConvert,devise1,devise2,result);
+    switchCurrency(formSwitch, devise1, devise2);
+
+    getCurrency(formConvert, devise1, devise2, result);
+
+
+} catch { }
+
+
+let divCard = document.querySelector(".cardJeux");
+
+let symbole = ["bitcoin", "dollar", "euro", "yen", "yen", "bitcoin", "dollar", "euro", "pound", "pound"];
+
+
+
+for (let i = 10; i >= 1; i--) {
+
+    let card = document.createElement("div");
+
+    card.setAttribute("class", "itemcard");
+
+    let rand = Math.floor(Math.random() * i);
+
+    card.dataset.nameSymbole = symbole[rand];
+
+    let svgCard = document.createElement("img");
+
+    svgCard.classList.add("svg-card");
+    svgCard.classList.add("hidden");
+
+    svgCard.setAttribute("src", `./assets/images/svg/${symbole[rand]}.svg`);
+
+
+    card.append(svgCard);
+
+    divCard.append(card);
+
+    symbole.splice(rand, 1);
+
+}
+
+let tabCard = document.querySelectorAll(".itemcard");
+
+let bloque = false;
+
+let carteRetournee = [];
+
+for (let card of tabCard) {
+
+    card.addEventListener("click", function () {
+
+        if (bloque) {
+
+            return;
+
+        }
+
+        let svg = this.children[0];
+
+        if (carteRetournee.includes(this)) {
+
+            return;
+
+        } else {
+
+
+            carteRetournee.push(this);
+            svg.classList.remove("hidden");
+
+        }
+
+
+        if (carteRetournee.length == 2) {
+
+            bloque = true;
+            verifCard();
+
+
+        }
+
+    })
+
+}
+
+
+function verifCard() {
+
+
+    if (carteRetournee[0].dataset.nameSymbole == carteRetournee[1].dataset.nameSymbole) {
+
+        carteRetournee[0].style.backgroundColor = "green";
+        carteRetournee[1].style.backgroundColor = "green";
+
+        setTimeout(() => {
+
+            carteRetournee[0].setAttribute("class", "hidden");
+            carteRetournee[1].setAttribute("class", "hidden");
+            carteRetournee = [];
+            bloque = false;
+
+        }, 1500);
+
+    } else {
+
+        setTimeout(() => {
+
+            carteRetournee[0].children[0].classList.add("hidden");
+            carteRetournee[1].children[0].classList.add("hidden");
+            carteRetournee = [];
+            bloque = false;
+
+        }, 1000);
+
+    }
+
+}
