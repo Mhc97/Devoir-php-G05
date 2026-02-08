1141a3d1891b227bdc6bddf1b0d167485b72637b
ismail <ismaildimah@gmail.com>
2026-02-08 12:52:45 +0100

    Ajout euroYen

--- euroYen.php
+++ euroYen.php
@@ -11,111 +11,69 @@ if (!connected($_SESSION)) {
     header("Location: ./login.php");
 }
 
-if (isset($_POST["nb1"]) && isset($_POST["nb2"])) {
 
-    if ($_POST["nb1"] != "" && $_POST["nb2"] != "") {
-
-
-        $_SESSION["operation"][] = multiplication($_POST["nb1"], $_POST["nb2"]);
-
-        $_SESSION["operationCount"]++;
-        $_SESSION["totalOperation"]++;
-    }
-};
 
 ?>
 
 <div class="calcul">
 
-    <h3>Multiplication</h3>
+    <h3>Conversion</h3>
 
 
+    <form id="formSwitch" method="post">
 
-    <form method="post">
-
-        <div class="input">
-
-
-            <label for="nb1" class="gris"> Nombre 1 </label>
-
-            <input type="number" name="nb1" placeholder="<?php if (isset($_POST["nb1"])  && isset($_POST["nb2"])):
-
-                                                                if ($_POST["nb1"] != "" && $_POST["nb2"] != ""):
-
-                                                                    echo $_POST["nb1"];
-
-                                                                else: ?>0<?php
-
-                                                                endif;
-
-                                                            else : ?>0<?php
-
-                                                            endif; ?>">
-
-        </div>
-
-        <div class="svg-container purple ">
+        <div class="switch">
 
 
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x text-purple-600" aria-hidden="true">
-                <path d="M18 6 6 18"></path>
-                <path d="m6 6 12 12"></path>
-            </svg>
+            <h1 class="devise" data-devise="eur"> Euros </h1>
+
+            <button type="submit">
+
+
+                <div class="svg-container green">
+
+
+                    <svg width="21px" height="21px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg">
+                        <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(4 2)">
+                            <path d="m4.5 8.5-4 4 4 4" />
+                            <path d="m12.5 12.5h-12" />
+                            <path d="m8.5.5 4 4-4 4" />
+                            <path d="m12.5 4.5h-12" />
+                        </g>
+                    </svg>
+
+                </div>
+
+            </button>
+
+
+            <h1 class="devise" data-devise="jpy">Yen</h1>
 
         </div>
-
-        <div class="input">
-
-            <label for="nb2" class="gris"> Nombre 2 </label>
-
-            <input type="number" name="nb2" placeholder="<?php if (isset($_POST["nb1"])  && isset($_POST["nb2"])):
-
-                                                                if ($_POST["nb1"] != "" && $_POST["nb2"] != ""):
-
-                                                                    echo $_POST["nb2"];
-
-                                                                else: ?>0<?php
-
-                                                                endif;
-
-                                                            else: ?>0<?php
-
-                                                            endif; ?>">
-
-
-        </div>
-
-        <div class="svg-container green">
-
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-equal text-green-600" aria-hidden="true">
-                <line x1="5" x2="19" y1="9" y2="9"></line>
-                <line x1="5" x2="19" y1="15" y2="15"></line>
-            </svg>
-
-        </div>
-
-        <div class="input">
-
-            <label for="resultat" class="gris"> Résultat </label>
-
-            <input type="number" name="resultat" readonly placeholder="<?php if (isset($_POST["nb1"]) && isset($_POST["nb2"])):
-
-                                                                            if ($_POST["nb1"] != "" && $_POST["nb2"] != ""):
-
-                                                                                echo (int)$_POST["nb1"] * (int)$_POST["nb2"];
-
-                                                                            else: ?>?<?php
-                                                                            endif; ?><?php else: ?>?<?php endif; ?>">
-
-
-        </div>
-
-        <button type="submit" class="blue"> Calculer </button>
 
     </form>
 
-    <h3 class="background"> 💡 La multiplication vous permet de multiplier deux nombres ensemble </h3>
+    <form id="formConvert" action="">
+
+
+        <div class="input">
+
+            <input type="number" placeholder="Veuillez introduire le montant a convertir">
+
+        </div>
+
+        <button type="submit" class="blue"> Convertir </button>
+
+
+    </form>
+
+    <div id="result">
+
+    
+
+    </div>
+
+    <h3 class="background"> 💡 Ce formulaire vous permet de convertir des euros en yens et inversement </h3>
 
 </div>
 
