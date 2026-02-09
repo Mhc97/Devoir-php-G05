56ddca0484941f6295d9f43023dd8df462eb56ea
ismail <ismaildimah@gmail.com>
2026-02-09 20:09:41 +0100

    debugg des erreurs js via try/catch

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,110 +17,114 @@ try {
 } catch { }
 
 
-let divCard = document.querySelector(".cardJeux");
-
-let symbole = ["bitcoin", "dollar", "euro", "yen", "yen", "bitcoin", "dollar", "euro", "pound", "pound"];
+try {
 
 
+    let divCard = document.querySelector(".cardJeux");
 
-for (let i = 10; i >= 1; i--) {
-
-    let card = document.createElement("div");
-
-    card.setAttribute("class", "itemcard");
-
-    let rand = Math.floor(Math.random() * i);
-
-    card.dataset.nameSymbole = symbole[rand];
-
-    let svgCard = document.createElement("img");
-
-    svgCard.classList.add("svg-card");
-    svgCard.classList.add("hidden");
-
-    svgCard.setAttribute("src", `./assets/images/svg/${symbole[rand]}.svg`);
+    let symbole = ["bitcoin", "dollar", "euro", "yen", "yen", "bitcoin", "dollar", "euro", "pound", "pound"];
 
 
-    card.append(svgCard);
 
-    divCard.append(card);
+    for (let i = 10; i >= 1; i--) {
 
-    symbole.splice(rand, 1);
+        let card = document.createElement("div");
 
-}
+        card.setAttribute("class", "itemcard");
 
-let tabCard = document.querySelectorAll(".itemcard");
+        let rand = Math.floor(Math.random() * i);
 
-let bloque = false;
+        card.dataset.nameSymbole = symbole[rand];
 
-let carteRetournee = [];
+        let svgCard = document.createElement("img");
 
-for (let card of tabCard) {
+        svgCard.classList.add("svg-card");
+        svgCard.classList.add("hidden");
 
-    card.addEventListener("click", function () {
-
-        if (bloque) {
-
-            return;
-
-        }
-
-        let svg = this.children[0];
-
-        if (carteRetournee.includes(this)) {
-
-            return;
-
-        } else {
+        svgCard.setAttribute("src", `./assets/images/svg/${symbole[rand]}.svg`);
 
 
-            carteRetournee.push(this);
-            svg.classList.remove("hidden");
+        card.append(svgCard);
 
-        }
+        divCard.append(card);
 
-
-        if (carteRetournee.length == 2) {
-
-            bloque = true;
-            verifCard();
-
-
-        }
-
-    })
-
-}
-
-
-function verifCard() {
-
-
-    if (carteRetournee[0].dataset.nameSymbole == carteRetournee[1].dataset.nameSymbole) {
-
-        carteRetournee[0].style.backgroundColor = "green";
-        carteRetournee[1].style.backgroundColor = "green";
-
-        setTimeout(() => {
-
-            carteRetournee[0].setAttribute("class", "hidden");
-            carteRetournee[1].setAttribute("class", "hidden");
-            carteRetournee = [];
-            bloque = false;
-
-        }, 1500);
-
-    } else {
-
-        setTimeout(() => {
-
-            carteRetournee[0].children[0].classList.add("hidden");
-            carteRetournee[1].children[0].classList.add("hidden");
-            carteRetournee = [];
-            bloque = false;
-
-        }, 1000);
+        symbole.splice(rand, 1);
 
     }
 
-}
+    let tabCard = document.querySelectorAll(".itemcard");
+
+    let bloque = false;
+
+    let carteRetournee = [];
+
+    for (let card of tabCard) {
+
+        card.addEventListener("click", function () {
+
+            if (bloque) {
+
+                return;
+
+            }
+
+            let svg = this.children[0];
+
+            if (carteRetournee.includes(this)) {
+
+                return;
+
+            } else {
+
+
+                carteRetournee.push(this);
+                svg.classList.remove("hidden");
+
+            }
+
+
+            if (carteRetournee.length == 2) {
+
+                bloque = true;
+                verifCard();
+
+
+            }
+
+        })
+
+    }
+
+
+    function verifCard() {
+
+
+        if (carteRetournee[0].dataset.nameSymbole == carteRetournee[1].dataset.nameSymbole) {
+
+            carteRetournee[0].style.backgroundColor = "green";
+            carteRetournee[1].style.backgroundColor = "green";
+
+            setTimeout(() => {
+
+                carteRetournee[0].setAttribute("class", "hidden");
+                carteRetournee[1].setAttribute("class", "hidden");
+                carteRetournee = [];
+                bloque = false;
+
+            }, 1500);
+
+        } else {
+
+            setTimeout(() => {
+
+                carteRetournee[0].children[0].classList.add("hidden");
+                carteRetournee[1].children[0].classList.add("hidden");
+                carteRetournee = [];
+                bloque = false;
+
+            }, 1000);
+
+        }
+
+    }
+}catch{}
